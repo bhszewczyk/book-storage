abe7bb84204a567ca53ccae7b59011d1ee7bde7c
Barbara Szewczyk <comamysza@gmail.com>
2022-09-22 16:12:29 +0200

    Add /books which lists all the books from database

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const path = require('path');
+const books = require('./books.json');
+
+// define absokute path to the views
+app.set('views', path.join(__dirname, 'views'));
+// set up engine as EJS
+app.set('view engine', 'ejs');
+
+app.get('/books', (req, res) => {
+	res.render('home', { books });
+});
 
 app.listen(PORT, () => {
 	console.log(`Listening to port ${PORT}...`);
+	// books.forEach((book) => {
+	// 	console.log(book.author);
+	// });
 });
