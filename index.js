ce867f23308864e13ef72caa7bf9a78610e35ed6
Barbara Szewczyk <comamysza@gmail.com>
2022-09-25 23:38:56 +0200

    Add details page for each book

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 3000;
 const path = require('path');
 const books = require('./books.json');
+const { v4: getUuid } = require('uuid');
 
 // define absokute path to the views
 app.set('views', path.join(__dirname, 'views'));
@@ -15,6 +16,14 @@ app.get('/', (req, res) => {
 
 app.get('/books', (req, res) => {
 	res.render('books', { books });
+});
+
+app.get('/book/:id', (req, res) => {
+	const { id } = req.params;
+	console.log(books[id]);
+	const book = books[id];
+
+	res.render('book', { book });
 });
 
 app.listen(PORT, () => {
